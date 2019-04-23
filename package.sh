fed6f674033cb83550a65ef1aacbd8b0e352c387
Dmitry Kryuchkov <dmitry.kryuchkov@cultureamp.com>
2019-04-23 14:43:57 +1000

    tweak package script

--- package.sh
+++ package.sh
@@ -3,3 +3,18 @@ set -e
 
 docker build . -t topcoder-summarization
 docker run -it --rm -v $PWD/data:/data topcoder-summarization
+
+mkdir -p .dist/solution
+mkdir -p .dist/code
+
+mv data/test/*_summary.txt .dist/solution/
+
+cp Dockerfile .dist/code/
+cp -r src .dist/code/
+
+pushd .dist
+zip -r submission.zip solution
+zip -r submission.zip code
+popd
+
+mv .dist/submission.zip .
