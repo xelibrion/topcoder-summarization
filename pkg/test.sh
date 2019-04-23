23d52b80c96b81e63d612fec99b54030d7dd5c74
Dmitry Kryuchkov <dmitry.kryuchkov@cultureamp.com>
2019-04-23 10:43:08 +1000

    add docker package

---
+++ pkg/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Inference"
+
+# A sample call to your testing script (single line):
+# ./test.sh /data/test/ val_summary.txt test_summary.txt
+# In this case you can assume that the testing data looks like this:
+#  data/
+#    test/
+#      test_article.txt
+
+#      val_article.txt
---
+++ pkg/train.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Training"
+
+# A sample call to your training script (single line):
+# ./train.sh /data/training/
+# In this case you can assume that the training data looks like this:
+#  data/
+#    training/
+#      train.bin
