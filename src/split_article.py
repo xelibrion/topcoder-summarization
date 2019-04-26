751a9f50c9cbdc56d3c3073d6964d26e82a92e77
Dmitry Kryuchkov <dmitry.kryuchkov@cultureamp.com>
2019-04-26 19:51:07 +1000

    modify split_article to include extra information

--- src/split_article.py
+++ src/split_article.py
@@ -3,8 +3,11 @@ import os
 from tqdm import tqdm
 import spacy
 import pandas as pd
+from pytorch_pretrained_bert import BertTokenizer
 
 from multiprocessing import Pool
+
+BERT_MODEL = 'bert-base-uncased'
 
 
 def rel_path(path, anchor=None):
@@ -16,14 +19,16 @@ def rel_path(path, anchor=None):
 
 
 def initializer():
-    global nlp
+    global nlp, tokenizer
     nlp = spacy.load('en_core_web_lg')
+    tokenizer = BertTokenizer.from_pretrained(BERT_MODEL)
 
 
 def split_sentence(input_tuple):
     article_id, article = input_tuple
     doc = nlp(article)
-    return [(article_id, sent.text) for sent in doc.sents]
+    return [(article_id, sent.text, len(tokenizer.tokenize(sent.text)))
+            for sent in doc.sents]
 
 
 df = pd.read_json(
@@ -41,13 +46,20 @@ with tqdm(total=len(articles)) as tq:
         args = list(enumerate(articles))
         for article_sentences in pool.imap_unordered(split_sentence, args):
             tq.update()
-            for t in article_sentences:
-                split_sentences.append(t)
+            for idx, t in enumerate(article_sentences):
+                art_id, text, num_tokens = t
+                split_sentences.append((art_id, idx, text, num_tokens))
 
         pool.close()
         pool.join()
 
-df_result = pd.DataFrame(split_sentences, columns=['article_id', 'sentence'])
+df_result = pd.DataFrame(split_sentences,
+                         columns=[
+                             'article_id',
+                             'sequence_id',
+                             'sentence',
+                             'num_bert_tokens',
+                         ])
 print(df_result.head(3))
 df_result.to_json(
     rel_path('../data/sentences.jsonl'),
