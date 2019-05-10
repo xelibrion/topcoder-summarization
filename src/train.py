19e4212d657a3f8394b58d812336e22032f0ef6e
Dmitry Kryuchkov <dmitry.kryuchkov@cultureamp.com>
2019-05-10 18:47:04 +1000

    use specific optimizer, refactor code to training loop

--- src/train.py
+++ src/train.py
@@ -3,24 +3,21 @@
 
 import os
 import random
-import math
-import time
 import argparse
 
-import numpy as np
 import pandas as pd
-import pyarrow.parquet as pq
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from torch.utils.data import DataLoader
 
-from tqdm import tqdm
+from pytorch_pretrained_bert.optimization import WarmupLinearSchedule
+from apex.optimizers import FP16_Optimizer
+from apex.optimizers import FusedAdam
 
 from nnet import Summarizer
-from nnet.meters import AverageMeter
 from nnet.dataset import SentencesDataset
+from training_loop import TrainigLoop, LearningRateScheduler
 
 MODEL_PATH = 'summarizer_model.pt'
 
@@ -33,101 +30,42 @@ torch.backends.cudnn.deterministic = True
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
+def build_optimizer(
+        model_params,
+        learning_rate,
+        warmup_proportion,
+        total_train_steps,
+):
+    # hack to remove pooler, which is not used
+    # thus it produce None grad that break apex
+    param_optimizer = [n for n in model_params if 'pooler' not in n[0]]
+
+    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [{
+        'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+        'weight_decay':
+        0.01
+    }, {
+        'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+        'weight_decay':
+        0.0
+    }]
+
+    optimizer = FusedAdam(
+        optimizer_grouped_parameters,
+        lr=learning_rate,
+        bias_correction=False,
+        max_grad_norm=1.0,
+    )
+    optimizer = FP16_Optimizer(optimizer, dynamic_loss_scale=True)
+    warmup_linear = WarmupLinearSchedule(warmup=warmup_proportion,
+                                         t_total=total_train_steps)
+
+    return optimizer, warmup_linear
 
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-
-def train_epoch(model, iterator, optimizer, criterion, epoch_steps=None):
-
-    loss_meter = AverageMeter(int(len(iterator) / 10))
-    model.train()
-
-    epoch_loss = 0
-
-    num_steps = epoch_steps or len(iterator)
-    steps_completed = 0
-
-    with tqdm(total=num_steps) as tq:
-        tq.set_description('Train')
-
-        for batch in iterator:
-            optimizer.zero_grad()
-
-            for idx, t in enumerate(batch):
-                batch[idx] = t.to(device)
-
-            input_ids, attention_mask, segments_ids, cls_ids, cls_mask, labels = batch
-            # labels = [batch_size x 512]
-
-            output = model(input_ids, attention_mask, segments_ids, cls_ids, cls_mask)
-            sent_scores, out_cls_mask = output
-
-            # sent_scores = [batch_size x 512]
-            # out_cls_mask = [batch_size x 512]
-
-            loss = criterion(sent_scores, labels)
-            loss = (loss * out_cls_mask.float()).sum()
-            (loss / loss.numel()).backward()
-
-            optimizer.step()
-
-            loss_meter.update(loss.item())
-            tq.set_postfix(loss='{:.3f}'.format(loss_meter.mavg), )
-            tq.update()
-
-            epoch_loss += loss.item()
-            steps_completed += 1
-            if steps_completed >= num_steps:
-                break
-
-    return epoch_loss / steps_completed
-
-
-def evaluate(model, iterator, criterion, epoch_steps=None):
-
-    model.eval()
-    loss_meter = AverageMeter(int(len(iterator) / 10))
-
-    epoch_loss = 0
-
-    num_steps = epoch_steps or len(iterator)
-    steps_completed = 0
-
-    with tqdm(total=num_steps) as tq:
-        tq.set_description('Validation')
-
-        with torch.no_grad():
-
-            for batch in iterator:
-                for idx, t in enumerate(batch):
-                    batch[idx] = t.to(device)
-
-                input_ids, attention_mask, segments_ids, cls_ids, cls_mask, labels = batch
-                # labels = [batch_size x 512]
-
-                output = model(input_ids, attention_mask, segments_ids, cls_ids, cls_mask)
-                sent_scores, out_cls_mask = output
-
-                loss = criterion(sent_scores, labels)
-                loss = (loss * out_cls_mask.float()).sum()
-
-                epoch_loss += loss.item()
-                loss_meter.update(loss.item())
-                tq.set_postfix(loss='{:.3f}'.format(loss_meter.mavg), )
-                tq.update()
-
-                steps_completed += 1
-                if steps_completed >= num_steps:
-                    break
-
-    return epoch_loss / steps_completed
 
 
 def compose_model():
@@ -167,46 +105,39 @@ def load_data(dataset_path, batch_size):
     return train_iterator, val_iterator
 
 
-def train(dataset_path, resume, lr, batch_size, epoch_steps=1000):
-
-    train_iterator, val_iterator = load_data(dataset_path, batch_size)
-
-    model, criterion = compose_model()
-    if resume:
-        model.load_state_dict(torch.load(MODEL_PATH))
-
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-
-    N_EPOCHS = 30
-
-    best_val_loss = float('inf')
-
-    for epoch in range(N_EPOCHS):
-
-        start_time = time.time()
-
-        train_loss = train_epoch(model, train_iterator, optimizer, criterion, epoch_steps)
-        val_loss = evaluate(model, val_iterator, criterion, epoch_steps)
-
-        end_time = time.time()
-
-        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            torch.save(model.state_dict(), MODEL_PATH)
-
-        print(f'\nEpoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
-        print(f'\tTrain Loss: {train_loss:.3f}')
-        print(f'\t Val. Loss: {val_loss:.3f}\n')
-
-
 def rel_path(path, anchor=None):
     if anchor is None:
         anchor = __file__
     anchor_path = os.path.abspath(anchor)
     anchor_dir = os.path.dirname(anchor_path)
     return os.path.abspath(os.path.join(anchor_dir, path))
+
+
+def save_checkpoint(model):
+    torch.save(model.state_dict(), MODEL_PATH)
+
+
+def train(dataset_path, resume, lr, batch_size, epochs=50, steps_per_epoch=1000):
+    train_iterator, val_iterator = load_data(dataset_path, batch_size)
+
+    model, criterion = compose_model()
+    if resume:
+        model.load_state_dict(torch.load(MODEL_PATH))
+
+    optimizer, schedule = build_optimizer(
+        list(model.named_parameters()),
+        lr,
+        0.1,
+        epochs * steps_per_epoch,
+    )
+
+    loop = TrainigLoop(
+        model,
+        optimizer,
+        lr_scheduler=LearningRateScheduler(schedule, lr),
+        callbacks={'on_best_val_loss': save_checkpoint},
+    )
+    loop.run(train_iterator, val_iterator, criterion)
 
 
 def main():
@@ -224,7 +155,7 @@ def main():
     parser.add_argument(
         '--lr',
         type=float,
-        default=1e-3,
+        default=5e-5,
     )
     parser.add_argument(
         '--batch-size',
---
+++ src/training_loop.py
@@ -0,0 +1,158 @@
+import time
+
+from tqdm import tqdm
+import torch
+
+from nnet.meters import AverageMeter
+
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class LearningRateScheduler(object):
+    """
+    :param schedule:  schedule (e.g. WarmupLinearSchedule) to get learning rate from
+    :param learning_rate: default learning rate
+    """
+
+    def __init__(self, schedule, learning_rate):
+        self.schedule = schedule
+        self.learning_rate = learning_rate
+
+    """
+    :param optimizer:  optimizer to tweak learning rate of
+    :param steps_completed: steps completed so far across all iterations
+    """
+
+    def update(self, optimizer, steps_completed):
+        lr_this_step = self.learning_rate * self.schedule.get_lr(steps_completed)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr_this_step
+
+
+class TrainigLoop(object):
+    def __init__(self, model, optimizer, lr_scheduler, callbacks):
+        self.model = model
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+        self.callbacks = callbacks
+        self.epoch_steps = 1000
+        self.total_steps_completed = 0
+
+    def run(self, train_iterator, val_iterator, criterion, epochs=30):
+        best_val_loss = float('inf')
+
+        for epoch in range(epochs):
+
+            start_time = time.time()
+
+            train_loss = self.train_epoch(train_iterator, criterion)
+            val_loss = self.evaluate(val_iterator, criterion)
+
+            end_time = time.time()
+
+            epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                self.callbacks['on_best_val_loss'](self.model)
+
+            print(f'\nEpoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+            print(f'\tTrain Loss: {train_loss:.3f}')
+            print(f'\t Val. Loss: {val_loss:.3f}\n')
+
+    def train_epoch(self, iterator, criterion):
+
+        loss_meter = AverageMeter(int(len(iterator) / 10))
+        self.model.train()
+
+        epoch_loss = 0
+
+        num_steps = self.epoch_steps or len(iterator)
+        steps_completed = 0
+
+        with tqdm(total=num_steps) as tq:
+            tq.set_description('Train')
+
+            for batch in iterator:
+                self.optimizer.zero_grad()
+
+                for idx, t in enumerate(batch):
+                    batch[idx] = t.to(device)
+
+                input_ids, attention_mask, segments_ids, cls_ids, cls_mask, labels = batch
+                # labels = [batch_size x 512]
+
+                output = self.model(input_ids, attention_mask, segments_ids, cls_ids,
+                                    cls_mask)
+                sent_scores, out_cls_mask = output
+
+                # sent_scores = [batch_size x 512]
+                # out_cls_mask = [batch_size x 512]
+
+                loss = criterion(sent_scores, labels)
+                loss = (loss * out_cls_mask.float()).sum()
+                (loss / loss.numel()).backward()
+
+                self.optimizer.step()
+
+                loss_meter.update(loss.item())
+                tq.set_postfix(loss='{:.3f}'.format(loss_meter.mavg), )
+                tq.update()
+
+                epoch_loss += loss.item()
+                steps_completed += 1
+                self.total_steps_completed += 1
+                self.lr_scheduler.update(self.optimizer, self.total_steps_completed)
+
+                if steps_completed >= num_steps:
+                    break
+
+        return epoch_loss / steps_completed
+
+    def evaluate(self, iterator, criterion):
+
+        self.model.eval()
+        loss_meter = AverageMeter(int(len(iterator) / 10))
+
+        epoch_loss = 0
+
+        num_steps = self.epoch_steps or len(iterator)
+        steps_completed = 0
+
+        with tqdm(total=num_steps) as tq:
+            tq.set_description('Validation')
+
+            with torch.no_grad():
+
+                for batch in iterator:
+                    for idx, t in enumerate(batch):
+                        batch[idx] = t.to(device)
+
+                    input_ids, attention_mask, segments_ids, cls_ids, cls_mask, labels = batch
+                    # labels = [batch_size x 512]
+
+                    output = self.model(input_ids, attention_mask, segments_ids, cls_ids,
+                                        cls_mask)
+                    sent_scores, out_cls_mask = output
+
+                    loss = criterion(sent_scores, labels)
+                    loss = (loss * out_cls_mask.float()).sum()
+
+                    epoch_loss += loss.item()
+                    loss_meter.update(loss.item())
+                    tq.set_postfix(loss='{:.3f}'.format(loss_meter.mavg), )
+                    tq.update()
+
+                    steps_completed += 1
+                    if steps_completed >= num_steps:
+                        break
+
+        return epoch_loss / steps_completed
